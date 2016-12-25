aa9f4dc3b187c2ea443392e508f526d24ee3cfe3
Pavol Hejný <me@pavolhejny.cz>
2016-12-25 22:23:55 +0100

    json test

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
     // process HTTP request. Since we're writing just WebSockets server
     // we don't have to implement anything.
+
+    response.end(JSON.stringify(galleries,null,4));
 });
 server.listen(1338, function() { });
 
@@ -12,15 +14,81 @@ wsServer = new WebSocketServer({
     httpServer: server
 });
 
+
+var galleries = {};
+
+
 // WebSocket server
 wsServer.on('request', function(request) {
     var connection = request.accept(null, request.origin);
+
+
+
+    var gallery;// = 'xxx'.Math.random();
+    var session = 'xxx'+Math.random();
+
+
+
+
+    /*setInterval(function () {
+        connection.sendUTF('KUK');
+    },1000);*/
+
+
+
+
+
 
     // This is the most important callback for us, we'll handle
     // all messages from users here.
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
             // process WebSocket message
+
+            try{
+
+                var message = JSON.parse(message.utf8Data);
+
+            }catch(error){
+
+                connection.sendUTF('Wrong message!');
+
+            }
+
+
+
+
+
+            if(message.type=='initial'){
+
+                if(typeof message.gallery!=='string'){
+                    connection.sendUTF('Initial request should contain gallery.');
+                    return;
+                }
+
+
+                gallery = message.gallery;
+                //session = 'xxx'+Math.random();//todo guid
+
+                galleries[gallery] = galleries[gallery] || {};
+                galleries[gallery][session] = galleries[gallery][session] || {};
+
+                connection.sendUTF('You are in gallery '+gallery+' with session '+session);
+
+            }else
+            if(message.type=='position'){
+
+                galleries[gallery][session].position = message.position;
+
+            }
+
+
+
+            //console.log(message);
+            //connection.sendUTF('You said: '+message.utf8Data);
+
+
+
         }
     });
 
